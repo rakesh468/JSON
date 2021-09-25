aec088784b596fa57180d15134916e9865e419ac
Rakesh Kumar <91141164+rakesh468@users.noreply.github.com>
2021-09-25 10:53:56 -0700

    task 3 JSON

---
+++ iteration.js
@@ -0,0 +1,23 @@
+// iteration using (for in loop)//
+const person={firstname:"rakesh",lastname:"kumar",age:22}
+let text="";
+for(let x in person ){
+    text=+person[x]
+}
+console.log(person);
+
+//iteration using for of loop//
+ var cars=["bmw","audi","benz"];
+ let text="";
+ for (let x of cars ){
+     text+=cars[x];
+     console.log(cars);
+}
+//iteration  using for loop//
+var array=[56,89,76,98]
+for(i=0;i<array.length;i++){
+    console.log(array);
+}
+
+
+ 
